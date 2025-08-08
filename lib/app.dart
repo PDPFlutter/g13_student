a666477bcc7ad01f6a7b419cb876a15631cdc226
Nasibali Abdiyev <abdiyevnasibali@gmail.com>
2025-08-08 17:24:50 +0500

    [005] add new file: home.dart, separate codes ...

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:g13_student/screen/home.dart';
 
-import 'main.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        //hello world
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter G13'),
     );
   }
 }
